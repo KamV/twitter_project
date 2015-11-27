11672a5f52404a0f1a898b8ca0864664c1c02c25
Kamil <valiullin2716@gmail.com>
2015-11-27 16:51:07 +0300

    Add tweet getting

--- build/app.js
+++ build/app.js
@@ -36572,6 +36572,39 @@ $provide.value("$locale", {
 
 !window.angular.$$csp().noInlineStyle && window.angular.element(document.head).prepend('<style type="text/css">@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>');
 
+ var user = [];
+ (function(){
+   var app = angular.module("twitter_user_list",[]);
+   app.controller("userOptions",function($scope){
+     $scope.users = user;
+     this.myclick = function(){
+        OAuth.initialize('rMEzxovyFqog3wQmXNu9DS69ICs');
+        OAuth.popup('twitter').done(function(result){
+          result.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+document.getElementById("account_name").value).done(function(data) {
+              user[user.length] = {
+                name: data.name,
+                followers: data.followers_count,
+                following: data.friends_count,
+                status: data.description
+              };
+              document.getElementById("account_name").value = "";
+              document.getElementById("tweet").innerHTML = data[0].text;
+              console.log(data);
+              $scope.users = user;
+              $scope.$apply();
+            })
+        });
+     }
+   });
+
+   user[user.length] = {
+     name: "Вася",
+     followers: "5",
+     following: "4",
+     status: "Прив"
+   };
+ })();
+
  function myFunction(){
    document.getElementById("followers").innerHTML = " ";
    document.getElementById("following").innerHTML = " ";
@@ -36579,11 +36612,11 @@ $provide.value("$locale", {
    OAuth.initialize('rMEzxovyFqog3wQmXNu9DS69ICs');
    //OAuth.create("twitter");
    OAuth.popup('twitter').done(function(result){
-      result.get('https://api.twitter.com/1.1/users/show.json?screen_name='+document.getElementById("account_name").value).done(function(data) {
-        document.getElementById("followers").innerHTML = data.followers_count;
+      result.get('https://api.twitter.com/1.1/statuses/home_timeline.json').done(function(data) {
+        document.getElementById("followers").innerHTML = data.text;
         document.getElementById("following").innerHTML = data.friends_count;
         document.getElementById("status").innerHTML = data.description;
       })
   });
 
- }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,39 @@
 
 //= ../../bower_components/angular/angular.js
 
+ var user = [];
+ (function(){
+   var app = angular.module("twitter_user_list",[]);
+   app.controller("userOptions",function($scope){
+     $scope.users = user;
+     this.myclick = function(){
+        OAuth.initialize('rMEzxovyFqog3wQmXNu9DS69ICs');
+        OAuth.popup('twitter').done(function(result){
+          result.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+document.getElementById("account_name").value).done(function(data) {
+              user[user.length] = {
+                name: data.name,
+                followers: data.followers_count,
+                following: data.friends_count,
+                status: data.description
+              };
+              document.getElementById("account_name").value = "";
+              document.getElementById("tweet").innerHTML = data[0].text;
+              console.log(data);
+              $scope.users = user;
+              $scope.$apply();
+            })
+        });
+     }
+   });
+
+   user[user.length] = {
+     name: "Вася",
+     followers: "5",
+     following: "4",
+     status: "Прив"
+   };
+ })();
+
  function myFunction(){
    document.getElementById("followers").innerHTML = " ";
    document.getElementById("following").innerHTML = " ";
@@ -10,11 +43,11 @@
    OAuth.initialize('rMEzxovyFqog3wQmXNu9DS69ICs');
    //OAuth.create("twitter");
    OAuth.popup('twitter').done(function(result){
-      result.get('https://api.twitter.com/1.1/users/show.json?screen_name='+document.getElementById("account_name").value).done(function(data) {
-        document.getElementById("followers").innerHTML = data.followers_count;
+      result.get('https://api.twitter.com/1.1/statuses/home_timeline.json').done(function(data) {
+        document.getElementById("followers").innerHTML = data.text;
         document.getElementById("following").innerHTML = data.friends_count;
         document.getElementById("status").innerHTML = data.description;
       })
   });
 
- }
+}
